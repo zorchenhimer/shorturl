049ced852a12dc59a80ece934be06ebcfc34b3cf
Nick Scheibenpflug <zorchenhimer@gmail.com>
2015-03-14 01:32:33 -0400

    Added ability to redirect links with /<shorturl> instead of /l/<shorturl>.

--- shorturl/urls.py
+++ shorturl/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from shortener.views import redirect
 
 urlpatterns = patterns('',
     url(r'^$', include('shortener.urls', namespace='shortener')),
@@ -7,4 +8,5 @@ urlpatterns = patterns('',
     url(r'^stats/', include('shortener.urls_stats', namespace='shortener')),
     url(r'^add/', include('shortener.urls_add', namespace='shortener')),
     url(r'^l/', include('shortener.urls_link', namespace='shortener')),
+    url(r'^(?P<lid>[a-zA-Z0-9]{10})$', redirect), 
 )
