95bf2afd23c2819276979d3fd422d06b6bcbcfd7
Nick Scheibenpflug <zorchenhimer@gmail.com>
2015-03-14 18:18:46 -0400

    Sayaka because reasons.

---
+++ static/scripts.js
@@ -0,0 +1,35 @@
+runesenabled = false;
+
+function enable_runes() {
+    music_tags = document.getElementsByTagName('h1');
+    runetags = document.getElementsByClassName('text');
+    for(var i = 0; i < music_tags.length; i++) {
+        music_tags[i].className = 'musicrunes';
+    }
+    for(var i = 0; i < runetags.length; i++) {
+        runetags[i].className = 'runes';
+    }
+}
+
+function disable_runes() {
+    music_tags = document.getElementsByTagName('h1');
+    runetags = document.getElementsByClassName('runes');
+    for(var i = 0; i < music_tags.length; i++) {
+        music_tags[i].className = '';
+    }
+    for(var i = 0; i < runetags.length; i++) {
+        runetags[i].className = 'text';
+    }
+}
+
+function toggle_runes() {
+    if(runesenabled == false) {
+        enable_runes();
+        runesenabled = true;
+    } else {
+        disable_runes();
+        runesenabled = false;
+    }
+}
+
+
